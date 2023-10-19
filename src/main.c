b94d4d5596eff2724e9493dc8d69090a0fa234a0
decimatedTomato <greentomatopower@gmail.com>
2023-10-19 18:57:27 +0200

    Added SDL main event loop

--- src/main.c
+++ src/main.c
@@ -4,14 +4,33 @@
 #include <stdint.h>
 #include <string.h>
 
-#include "snake.h"
-
 // GRAPHICS
 #include <SDL2/SDL.h>
 
+#define WIDTH 1280
+#define HEIGHT 720
+
 int main(int argc, char *argv[]) {
     
-    printf("hello world\n");
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    if (window == NULL) {
+        fprintf(stderr, "Failed to create window: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
     
-    return 0;
+    SDL_Event windowEvent;
+
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            if (windowEvent.type == SDL_QUIT) break;
+        }
+    }
+
+    printf("Closing window");
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
